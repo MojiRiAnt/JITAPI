1f84c66f8d4f6c084281f76ae1517b85b79af79c
MojiRiAnt <DmiMatth@gmail.com>
2018-08-05 09:41:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,33 @@ import flask
 
 app = flask.Flask(__name__)
 
-# Configuration stuff here
+# ====== Configuration stuff here ======
 
-# Crypto stuff here
+app.config.update
+(
+    SQLALCHEMY_DATABASE_URI="sqlite:///var/database.db",
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    FLASK_ENV="development"
+)
 
-# Database stuff here
 
-# Routes & queries here
+# ====== Crypto stuff here ======
+
+
+
+# ====== Database stuff here ======
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = flask.SQLAlchemy()
+
+
+# ====== Routes & queries here ======
 
 @app.route('/test')
 def testroute():
 	return "<h1>Welcome to the test route! Danik sosi</h1>"
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='5000', debug=True) # WARNING : Debug mode is ON
